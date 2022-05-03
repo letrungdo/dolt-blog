9e1c20b0f25752859725143f1aba9a570b89f56a
DoLT <dolt@runsystem.net>
2022-05-03 10:43:00 +0700

    remove cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,11 +63,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-netlify`,
       options: {
-        headers: {
-          "/*.js": ["cache-control: public, max-age=31536000, immutable"],
-          "/*.css": ["cache-control: public, max-age=31536000, immutable"],
-          "/sw.js": ["cache-control: public, max-age=0, must-revalidate"],
-        },
+        headers: {},
       },
     },
     {
